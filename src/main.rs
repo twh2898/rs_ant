c45d75800417fbfd808129fc6a900fe3d8a92007
Thomas Harrison <twh2898@vt.edu>
2017-06-20 00:14:11 -0400

    Added glium and created basic render loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 
+#[macro_use]
+extern crate glium;
+
+mod support;
 mod util;
 mod world;
 
@@ -6,14 +10,46 @@ use util::console::clear_console;
 use world::World;
 
 fn main() {
-    // clear_console();
+    use glium::DisplayBuild;
+
+    let display = glium::glutin::WindowBuilder::new()
+        .with_vsync()
+        .with_dimensions(1024, 760)
+        .with_title("Ant".to_string())
+        .build_glium()
+        .expect("Couldnot create display!");
+
+    let program = support::shaders::load_program(&display);
+
     let mut world = World::new();
     world.generate();
 
-    for _ in 0..200 {
-        world.step();
-        clear_console();
-        println!("{}", world);
-        std::thread::sleep(std::time::Duration::from_millis(500));
-    }
+    let steptime = 1000.;
+    let mut elapse = 0.;
+
+    support::start_loop(|delta| {
+        use glium::Surface;
+        use glium::glutin::Event;
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+
+        elapse += delta;
+
+        if elapse > steptime {
+            world.step();
+            elapse = 0.;
+        }
+
+        target.finish().expect("Could not finish! ;)");
+
+        for ev in display.poll_events() {
+            match ev {
+                Event::Closed => return support::Action::Stop,
+                _ => (),
+            }
+        }
+
+        support::Action::Continue
+    });
 }
