dc7d9a57efaa8484267486a1e71f65b3837705a6
Thomas Harrison <twh2898@vt.edu>
2017-06-21 13:12:32 -0400

    Updated shaders to include colors

--- src/support/shaders.rs
+++ src/support/shaders.rs
@@ -11,18 +11,23 @@ pub const vertex_shader_src: &'static str = r#"
      #version 140
 
     in vec2 position;
+    in vec3 color;
+
+    out vec3 o_color;
 
     void main() {
         gl_Position = vec4(position, 0.0, 1.0);
+        o_color = color;
     }
 "#;
 
 pub const fragment_shader_src: &'static str = r#"
     #version 140
 
+    in vec3 o_color;
     out vec4 color;
 
     void main() {
-        color = vec4(1.0, 0.0, 0.0, 1.0);
+        color = vec4(o_color, 1.0);
     }
 "#;
