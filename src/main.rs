b97303e7d2d0594ae7036610c46fd495112dac35
Thomas Harrison <twh2898@vt.edu>
2017-06-21 19:57:23 -0400

    Removed Hello Triangle

--- src/main.rs
+++ src/main.rs
@@ -27,24 +27,13 @@ fn main() {
 
     let program = support::shaders::load_program(&display).unwrap();
 
-    // Load Indecies
+    // Load Vertex Buffers
 
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-    let line_indices = glium::index::NoIndices(glium::index::PrimitiveType::LinesList);
-
-    // Load Vertex Buffer
-
-    let mut grid = grid::Grid::new()
+    let grid = grid::Grid::new()
         .with_cols(160)
         .with_rows(100)
         .with_border(false)
         .with_color([0.2, 0.2, 0.2]);
-
-    let vertex1 = Vertex::new(-1., -1., [1., 0., 0.]);
-    let vertex2 = Vertex::new(0.0, 0.5, [0., 1., 0.]);
-    let vertex3 = Vertex::new(0.5, -0.25, [0., 0., 1.]);
-    let shape = vec![vertex1, vertex2, vertex3];
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
 
     // Time Control
 
@@ -63,14 +52,6 @@ fn main() {
             println!("Step");
             elapse = 0.;
         }
-
-        target
-            .draw(&vertex_buffer,
-                  &indices,
-                  &program,
-                  &glium::uniforms::EmptyUniforms,
-                  &Default::default())
-            .unwrap();
 
         target
             .draw(&grid.vertex_buffer(&display).unwrap(),
