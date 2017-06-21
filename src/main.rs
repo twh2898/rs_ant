73ea1571e5dc69df3f6ef30c129eb7241812f6bb
Thomas Harrison <twh2898@vt.edu>
2017-06-20 22:35:33 -0400

    Added drawing of simple triangle

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,23 @@ mod world;
 use util::console::clear_console;
 use world::World;
 
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+impl Vertex {
+    fn new() -> Vertex {
+        Vertex { position: [0.0, 0.0] }
+    }
+
+    fn from_points(x: f32, y: f32) -> Vertex {
+        Vertex { position: [x, y] }
+    }
+}
+
 fn main() {
     use glium::DisplayBuild;
 
@@ -19,7 +36,14 @@ fn main() {
         .build_glium()
         .expect("Couldnot create display!");
 
-    let program = support::shaders::load_program(&display);
+    let program = support::shaders::load_program(&display).unwrap();
+
+    let vertex1 = Vertex::from_points(-1., -1.);
+    let vertex2 = Vertex::from_points(0.0, 0.5);
+    let vertex3 = Vertex::from_points(0.5, -0.25);
+    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     let mut world = World::new();
     world.generate();
@@ -40,6 +64,14 @@ fn main() {
             world.step();
             elapse = 0.;
         }
+
+        target
+            .draw(&vertex_buffer,
+                  &indices,
+                  &program,
+                  &glium::uniforms::EmptyUniforms,
+                  &Default::default())
+            .unwrap();
 
         target.finish().expect("Could not finish! ;)");
 
